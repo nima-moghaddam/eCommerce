30c047a4ddcb845a58fdf6ac763136cf58e2cac3
Nima <nima.73sm@yahoo.com>
2022-05-29 17:52:27 +0430

    configured overall font size and removed tap color in phone mode

--- src/components/products/ProductCard.js
+++ src/components/products/ProductCard.js
@@ -9,25 +9,25 @@ import RatingBadge from "./../../Ui/badge/RatingBadge";
 
 const ProductCard = ({ image, price, count, title, id, rating }) => {
   return (
-    <div className="flex max-h-72 cursor-pointer flex-col rounded bg-white p-1 shadow-md transition-all duration-300 ease-linear hover:scale-105 md:p-1">
+    <div className="flex max-h-80 cursor-pointer flex-col rounded bg-white p-1 shadow-md transition-all duration-300 ease-linear hover:scale-105 md:p-1">
       <Link to={`/details/${id}`}>
         <div className="my-3 flex w-full justify-center">
           <img src={image} alt={title} className="h-28 w-24" />
         </div>
-        <p className="mx-2 mb-3 truncate text-center font-serif text-xs text-gray-600 ">
+        <p className="mx-2 mb-3 truncate text-center font-serif text-sm text-gray-600 md:text-lg ">
           {title}
         </p>
-        <div className="flex justify-around">
-          <p className="whitespace-nowrap text-xs font-thin leading-6">
+        <div className="mb-2 flex justify-around">
+          <p className="whitespace-nowrap text-sm font-thin leading-6 md:text-base">
             only <strong>{count}</strong> in storage
           </p>
         </div>
-        <div className="flex flex-row items-center justify-around">
+        <div className="flex flex-row items-center justify-around mb-1">
           <RatingBadge>{rating}</RatingBadge>
           <PriceBadge>{price}</PriceBadge>
         </div>
       </Link>
-      <div className="flex justify-center p-2">
+      <div className="mb-2 flex justify-center p-2">
         <CartBtn id={id} />
         <FavouriteBtn id={id} />
       </div>
--- src/components/products/ProductDetail.js
+++ src/components/products/ProductDetail.js
@@ -24,8 +24,8 @@ const ProductDetail = () => {
   const { count, rate } = rating;
 
   return (
-    <section className="mt-48 p-6 md:mt-16 md:p-24">
-      <div className="relative flex h-auto flex-col rounded bg-white p-3 shadow-md md:flex-row">
+    <section className="mx-auto mt-40 w-full p-6 md:mt-40 md:p-24 lg:w-[60rem]">
+      <div className="relative flex h-auto flex-col rounded-md bg-white p-3 shadow-md md:flex-row md:p-6">
         <div className="m-auto flex w-full justify-center p-2  md:w-1/3">
           <img
             src={image}
@@ -34,15 +34,15 @@ const ProductDetail = () => {
           />
         </div>
         <div className="mb-2 flex w-full flex-col p-3 md:ml-4 md:w-2/3">
-          <h3 className="text-md font-mono font-bold">{title}</h3>
-          <span className="mt-1 mb-3 text-xs font-semibold text-slate-400">
+          <h3 className="font-sans text-lg font-bold">{title}</h3>
+          <span className="mt-1 mb-3 text-base font-semibold text-slate-400">
             Product-Code : {category}-{id}
           </span>
-          <p className="text-xs font-thin leading-6">
+          <p className="text-base font-thin leading-6">
             <strong>{count}</strong> available in storage
           </p>
           <div className="my-2 flex content-center  items-center">
-            <p className="flex font-mono text-sm font-bold">
+            <p className="flex text-sm font-bold">
               Price :
               <span className="ml-1">
                 <PriceBadge>{price}</PriceBadge>
@@ -52,10 +52,12 @@ const ProductDetail = () => {
               <RatingBadge>{rate}</RatingBadge>
             </div>
           </div>
-          <h4 className="my-2 font-mono font-bold text-red-300 underline underline-offset-2">
+          <h4 className="my-2 font-sans text-xl font-bold text-red-300 underline underline-offset-4">
             Descriptions :
           </h4>
-          <p className="text-justify text-xs font-normal">{description}</p>
+          <p className="text-justify text-base font-normal">
+            {description}
+          </p>
           <div className="my-5 flex items-center justify-start">
             <FavouriteBtn id={id} />
             <CartBtn id={id} />
@@ -63,7 +65,7 @@ const ProductDetail = () => {
         </div>
         <button
           onClick={() => navigate(-1)}
-          className="text-whiteshadow-md absolute bottom-2 right-2 rounded-sm border border-red-300 px-2 pb-1 font-bold text-red-300 transition-all duration-200 ease-linear hover:bg-red-500 hover:text-white"
+          className="absolute bottom-3 right-3 flex items-center justify-center rounded-md border border-red-300 px-8 py-2 font-bold text-red-300 shadow-md transition-all duration-200 ease-linear hover:bg-red-500 hover:text-white md:px-2"
         >
           Return
         </button>
